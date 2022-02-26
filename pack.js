504d5c4a05c9e23a3f1ccee4bc60441787284f42
leonwgc <giantfish@126.com>
2022-02-26 09:28:38 +0800

    chore: update

--- pack.js
+++ pack.js
@@ -5,26 +5,20 @@ const path = require('path');
 
 const isBuild = !!argv.build;
 
-pack(
-  !isBuild,
-  {
-    entry: {
-      index: `./demo/index`,
-    },
-    output: {
-      path: path.resolve(__dirname, 'docs'),
-    },
-    devServer: {
-      port: 9200,
-    },
-    resolve: {
-      alias: {
-        'w-touch': path.resolve(__dirname, './src'),
-      },
+pack(!isBuild, {
+  entry: {
+    index: `./demo/index`,
+  },
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    publicPath: '',
+  },
+  devServer: {
+    port: 9200,
+  },
+  resolve: {
+    alias: {
+      'w-touch': path.resolve(__dirname, './src'),
     },
   },
-  () => {
-    // eslint-disable-next-line no-console
-    console.log('build');
-  }
-);
+});
